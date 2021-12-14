6088d4debac3e94613fba87f6585f266d401d940
Your Name <you@example.com>
2021-12-13 23:19:37 -0300

    agregue carrito

--- src/app.js
+++ src/app.js
@@ -11,3 +11,4 @@ app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.ht
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
 app.get('/login/', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
 app.get('/detalle', (req, res) => res.sendFile(path.resolve(__dirname, './views/detalle.html')));
+app.get('/carrito', (req, res) => res.sendFile(path.resolve(__dirname, './views/carrito.html')));
