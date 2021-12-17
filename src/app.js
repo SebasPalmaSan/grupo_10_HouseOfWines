67883c55082d59c47be785eeefb1b31ce3b48575
SebasPalmaSan <sebapalma59@gmail.com>
2021-12-16 23:38:46 -0300

    mas nuevo

--- src/app.js
+++ src/app.js
@@ -9,6 +9,6 @@ app.listen(3080, () => console.log("listening on port http://localhost:" + app.s
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
-app.get('/login/', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
 app.get('/detalle', (req, res) => res.sendFile(path.resolve(__dirname, './views/detalle.html')));
 app.get('/carrito', (req, res) => res.sendFile(path.resolve(__dirname, './views/carrito.html')));
