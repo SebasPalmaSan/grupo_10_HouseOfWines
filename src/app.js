28629e43b5ad1ac58870a1cc98c3ce413b5f38d8
MelisaMendez <meli68921992@gmail.com>
2021-12-17 00:19:23 -0300

    este sisi

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3080
 app.set('port', port)
 
 
-app.use(express.static(path.resolve(__dirname, './public')))
+app.use(express.static(path.resolve(__dirname, '../public')))
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')))
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views', 'register.html')));
