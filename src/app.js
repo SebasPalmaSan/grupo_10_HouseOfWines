1513866b8ffe9a6ce37a20dcb15e8145ae0c462a
SantiagoGonzalezCalcagno <s.gonzalezcalcagno@gmail.com>
2022-02-01 19:47:52 -0300

    prueba de usuarios 01-02

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const cookie = require('cookie-parser');
 const method = require('method-override');
 const app = express();
 
@@ -12,18 +14,20 @@ app.listen(app.get('port'), () => console.log('Listen un http://localhost:' + ap
 
 
 app.use(express.static(path.resolve(__dirname, '../public/')));
-app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')));
-app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.resolve(__dirname, '../uploads')));
+app.use(express.urlencoded({extended: true}));  // req.body en los formularios
+app.use(session({secret:'HOW', resave: true, saveUnInitialized: false})); //req.session
+app.use(cookie()); // req.cookies //res.cookie('name', 'value', {expires: new Date(Date.now())})
 //app.use(express.json());
 
 app.use(method('m')); // ?m=PUT || ?m=DELETE
 
 
 
-app.use(require('./routes/main'));
+app.use('/', require('./routes/main'));
 app.use('/products', require('./routes/product'));
 app.use('/users', require('./routes/user'));
-app.use('/carrito', require('./routes/cart'));
-app.use('/file/', require('./routes/file'));
+//app.use('/carrito', require('./routes/cart'));
+//app.use('/file/', require('./routes/file'));
 //app.use('/detail', require('./routes/detail'));
 //app.use('/files', require('./routes/file'));
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,29 @@
 const {Router} = require('express');
 const router = Router();
-const {login, register} = require('../controllers/user');
+const {login, register, create, profile, logout, save, access, uploadPassword, uploadAvatar} = require('../controllers/user');
+const validatorSave = require('../middlewares/save')
+const path = require('path');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, path.resolve(__dirname, '../../uploads')),
+    filename: (req, file, cb) => cb(null, 'user-image-' + Date.now() + path.extname(file.originalname))
+})
+const upload = multer({storage});
 
+
+//get
 router.get('/login', login);
 router.get('/register', register);
+router.get('/profile', [], profile);
+router.get('/logout', [], logout);
+
+//post
+router.post('/save', /* validatorSave, */ save);
+router.post('/access', [], access);
+
+
+//put
+//router.put('/upload/password', [], uploadPassword);
+//router.put('/upload/avatar', [], uploadAvatar);
+
 module.exports = router
