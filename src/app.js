3942eed1f915fefa2e17c56fe98138f524d064ca
MelisaMendez <meli68921992@gmail.com>
2022-02-12 16:47:55 -0300

    agregando cookies y session

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const session = require ('express-session');
+const cookies = require ('cookie-parser');
 const path = require('path');
 const method = require('method-override');
 const app = express();
 
+app.use(session ({
+    secret: "sshh, it's a secret",
+    resave: false,
+    saveUninitialized: false,
+}))
+app.use(cookies);
+app.use(userLoggedMiddleware);
 
 app.set('view engine', 'ejs');
+
 app.set('views', path.resolve(__dirname, './views'));
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), () => console.log('Listen un http://localhost:' + app.get('port')));
-
-
 app.use(express.static(path.resolve(__dirname, '../public/')));
+
+
 app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')));
 app.use(express.urlencoded({extended: true}));
 //app.use(express.json());
