da2630d17f18c994e01eddcc51c137de778dbc6c
MelisaMendez <meli68921992@gmail.com>
2022-03-24 20:33:35 -0300

    Arreglos en el perfil de admin

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -1,25 +1,4 @@
 const db = require ('../database/models');
-
-/*const userLoggedMiddleware = (req, res, next) => {
-    db.User.findOne({ 
-        where: {
-            email: req.cookies && req.cookies.user ? req.cookies.user : null,
-        }
-    })
-    .then(users =>{
-        const userLogged = users;
-        if(req.session && req.session.user){
-            userLogged = req.session.user
-        }
-
-        res.locals.user = userLogged;
-
-        next();
-    })
-    .catch(error => res.send(error))
-}*/
-
-
 const userLoggedMiddleware = (req, res, next) => {
     db.User.findOne({ 
         where: {
