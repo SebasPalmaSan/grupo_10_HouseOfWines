41d429d5e0e099afec0b8aa4505bbc86bd1be7ef
SantiagoGonzalezCalcagno <s.gonzalezcalcagno@gmail.com>
2022-03-24 21:13:14 -0300

    imagenes en home

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -2,12 +2,14 @@ const db = require('../database/models')
 
 module.exports = {
     index: (req, res) => {
-        db.Product.findAll()
+    
+        db.Product.findAll({include: [{association:'image'}]})
         .then(function(products){
+          //  return res.send(products)
             res.render('home', {
         styles: ['home'],
         title: 'House of Wines | Home',
-        products: products
+        products: products,
         })
     })
 }
