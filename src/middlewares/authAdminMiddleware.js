14891b4c26311d0351a7596c15c11297bf07d80f
MelisaMendez <meli68921992@gmail.com>
2022-04-06 13:57:31 -0300

    authAdminMiddleware

--- src/middlewares/authAdminMiddleware.js
+++ src/middlewares/authAdminMiddleware.js
@@ -2,7 +2,7 @@ function authAdminMiddleware(req,res,next) {
     
     let userLogged = req.session != undefined && req.session.userLogged ? req.session.userLogged : null;
 
-    return (!userLogged) ? res.redirect('/users/errorLogueado') : (userLogged.isAdmin == 1) ? next() : res.redirect('/');
+    return (!userLogged) ? res.redirect('/users/login') : (userLogged.isAdmin == 1) ? next() : res.redirect('/users/errorLogueado');
 
 }
 
